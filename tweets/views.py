cf88315c477a82090ee00a4db243f5937073f75c
skuttenkuler <sam.kuttenk@gmail.com>
2020-05-16 14:02:43 -0700

    db api get and json response working and displaying, base and home templates created

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,37 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 # Create your views here.
 
+from .models import Tweet
+
+
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1> Hey world</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    query = Tweet.objects.all()
+    tweets_list = [{"id": tweet.id, "content": tweet.content} for tweet in query]
+    data = {
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API
+    """
+    
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.object.get(id=tweet_id)
+        data['content'] =obj.content
+    except:
+        data['message'] = "Not found"
+        status = 400
+
+
+    
+    return JsonResponse(data, status=status)
